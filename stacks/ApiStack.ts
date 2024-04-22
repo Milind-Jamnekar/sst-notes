c2c083bf0960585cf58920c58be9c12c5bd5c073
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-04-22 22:34:50 +0530

    api added

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,24 @@
+import { Api, StackContext, use } from "sst/constructs";
+import { StorageStack } from "./MyStorage";
+
+export function ApiStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
+
+  //   api is c`reated
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: { bind: [table] },
+    },
+    routes: {
+      "POST /notes": "packages/functions/src/create.main",
+      "GET /notes/{id}": "packages/functions/src/get.main",
+      "GET /notes": "packages/functions/src/list.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
+      "DELETE /notes/{id}": "packages/functions/src/delete.main",
+    },
+  });
+
+  stack.addOutputs({ ApiEndpoint: api.url });
+
+  return { api };
+}
